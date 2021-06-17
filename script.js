36806239e1e8c516fcdfdbef8f1d24f00912b064
oscarortiz-mondragon <66816556+oscarortiz-mondragon@users.noreply.github.com>
2021-06-17 01:25:20 -0500

    More lively choice selection.

--- script.js
+++ script.js
@@ -24,7 +24,118 @@ timeLine.to('.options',{x:'0', duration:1, delay:3})
 )
 
 
+let none = document.querySelector('.none');
+let light = document.querySelector(".light")
+let mod = document.querySelector('.mod')
+let heavy = document.querySelector('.heavy')
 
 
+ let response = document.getElementById("response")
+
+// NONE
+none.addEventListener("click",function(){
+timeLine.to('.light',{
+  x: "-100%",duration:.3
+})
+timeLine.to('.mod',{
+  x: "-100%",duration:.3, 
+})
+timeLine.to('.heavy',{
+  x: "-100%",duration:.3, 
+})
+timeLine.to('.kidding',{y:"-100%",opacity:0})
+
+timeLine.to('.confirm',{opacity:1})
+})
+
+// Light
+light.addEventListener("click",function(){
+
+timeLine.to('.none',{
+  x: "-100%",duration:.3 
+})
+timeLine.to('.light',{
+  y:"-100%", duration:.3})
+timeLine.to('.mod',{
+  x: "-100%",duration:.3, 
+})
+timeLine.to('.heavy',{
+  x: "-100%",duration:.3, 
+})
+
+timeLine.to('.kidding',{y:"-100%",opacity:0})
+
+timeLine.to('.confirm',{opacity:1})
+})
+// MOD 
+mod.addEventListener("click",function(){
+
+timeLine.to('.none',{
+  x: "-100%",duration:.3
+})
+timeLine.to('.light',{
+  x: "-100%",duration:.3, 
+})
+timeLine.to('.mod',{
+  y:"-250%", duration:.3})
+
+timeLine.to('.heavy',{
+  x: "-100%",duration:.3, 
+})
+
+timeLine.to('.kidding',{y:"-100%",opacity:0})
+
+timeLine.to('.confirm',{opacity:1})
+})
+// HEAVY
+heavy.addEventListener("click",function(){
+ 
+timeLine.to('.kidding',{y:"-100%",opacity:0},"-=1")
+timeLine.to('.none',{
+  x: "-100%",duration:.3
+})
+
+timeLine.to('.light',{
+  x: "-100%",duration:.3, 
+})
 
 
+timeLine.to('.mod',{
+  x:"-100%", duration:.3})
+ timeLine.to('.heavy',{
+  y: "-400%",duration:.3})
+
+
+timeLine.to('.confirm',{opacity:1})
+timeLine.to('.confirmIcons',{
+  x:"0", duration:1
+})
+})
+
+let no = document.querySelector('.fa-window-close')
+let yes = document.querySelector('.fa-check-square')
+
+
+no.addEventListener("click", function(){
+timeLine.to('.confirm',{opacity:0})
+
+ timeLine.to('.heavy',{
+  x: "50%",duration:.2, y:"50"})
+
+
+timeLine.to('.mod',{
+  x:"50%", duration:.2, y: "50"})
+
+  timeLine.to('.light',{
+  x: "50%",duration:.2, y: "50" 
+})
+
+  timeLine.to('.none',{
+  x: "50%",duration:.2
+})
+
+timeLine.to('.kidding',{y:"50", opacity:1}, '-=1')
+
+timeLine.to('.confirm',{opacity:0})
+
+})
